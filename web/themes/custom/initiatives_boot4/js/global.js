6285146d36640c28b6ad8eebe20dd44c7ccb3183
Paul Foos <paul.foos@umb.edu>
2020-11-17 14:19:59 -0500

    updated IMCE, new custom bootstrap theme

--- web/themes/custom/initiatives_boot4/js/global.js
+++ web/themes/custom/initiatives_boot4/js/global.js
@@ -7,7 +7,7 @@
 
   'use strict';
 
-  Drupal.behaviors.mpte_boot4 = {
+  Drupal.behaviors.initiatives_boot4 = {
     attach: function (context, settings) { 
       if (!$('span.distNote').length) $('#block-schooldistricts h2').after(' <span class="distNote">NOTE: Schools districts listed below participate in one or more of the interagency groups listed on this site. Districts not shown do not participate in any of the groups listed.<hr /></span>');
 
