aa5ea94d0eb844e292fa3366701145225b1c7711
Paul Foos <paul.foos@umb.edu>
2020-12-08 13:04:35 -0500

    adding fonts

--- web/themes/custom/initiatives_boot4/js/global.js
+++ web/themes/custom/initiatives_boot4/js/global.js
@@ -10,26 +10,6 @@
   Drupal.behaviors.initiatives_boot4 = {
     attach: function (context, settings) { 
 
-
-      $('.order-first',context).once('sidebar').each(function(){
-        var m = window.location.search.indexOf("at_activities");
-        var n = window.location.search.indexOf("focus_areas");
-        var o = window.location.search.indexOf("state");
-        if(m > -1 || n > -1 || o > -1  ) {
-         
-            if($('#block-yoursearch .searchIntro').hasClass('homeShow')) $('.searchIntro').removeClass('homeShow');
-        
-
-          }
-        else {
-        
-          if(!$('#block-yoursearch .searchIntro').hasClass('homeShow')) $('.searchIntro').addClass('homeShow');
-        }
-
-
-
-      }
-      );
      
 
       $('.view-display-id-page_1',context).once('view-solr-search-content').each(function(){
