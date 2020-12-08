e64b60827314a3ddfc9dc21163bb53bfc8d15e0e
Paul Foos <paul.foos@umb.edu>
2020-12-08 11:17:44 -0500

    styles and js for hide/show of front page intro and search results

--- web/themes/custom/initiatives_boot4/js/global.js
+++ web/themes/custom/initiatives_boot4/js/global.js
@@ -9,8 +9,31 @@
 
   Drupal.behaviors.initiatives_boot4 = {
     attach: function (context, settings) { 
-      if (!$('span.distNote').length) $('#block-schooldistricts h2').after(' <span class="distNote">NOTE: Schools districts listed below participate in one or more of the interagency groups listed on this site. Districts not shown do not participate in any of the groups listed.<hr /></span>');
+     
 
+      $('.view-display-id-page_1',context).once('view-solr-search-content').each(function(){
+        var m = window.location.search.indexOf("at_activities");
+        var n = window.location.search.indexOf("focus_areas");
+        var o = window.location.search.indexOf("state");
+        if(m > -1 || n > -1 || o > -1  ) {
+            if ($('.view-header').hasClass('homeShow'))$('.view-header').removeClass('homeShow');
+            if ($('.view-content.row').hasClass('hideRow'))$('.view-content.row').removeClass('hideRow');
+            if ($('nav').hasClass('hideRow'))$('nav').removeClass('hideRow');
+            if ($('.searchJump').hasClass('searchJump'))$('.view-header').removeClass('searchJump');
+
+          }
+        else {
+          if(!$('.view-header').hasClass('homeShow'))$('.view-header').addClass('homeShow');
+          if(!$('.searchJump').hasClass('homeShow'))$('.searchJump').addClass('homeShow');
+          if(!$('.view-content.row').hasClass('hideRow'))$('.view-content.row').addClass('hideRow');
+          if(!$('.view-content.row').hasClass('hideRow'))$('.view-content.row').addClass('hideRow');
+          if(!$('nav').hasClass('hideRow'))$('nav').addClass('hideRow');
+        }
+
+
+
+      }
+      );
     }
   };
  
