a739e3e9574f0663dd9bb64d32e98643d14732e1
Paul Foos <paul.foos@umb.edu>
2020-12-08 12:23:03 -0500

    sidebar search summary tweaking

--- web/themes/custom/initiatives_boot4/js/global.js
+++ web/themes/custom/initiatives_boot4/js/global.js
@@ -9,6 +9,27 @@
 
   Drupal.behaviors.initiatives_boot4 = {
     attach: function (context, settings) { 
+
+
+      $('.order-first',context).once('sidebar').each(function(){
+        var m = window.location.search.indexOf("at_activities");
+        var n = window.location.search.indexOf("focus_areas");
+        var o = window.location.search.indexOf("state");
+        if(m > -1 || n > -1 || o > -1  ) {
+         
+            if($('#block-yoursearch .searchIntro').hasClass('homeShow')) $('.searchIntro').removeClass('homeShow');
+        
+
+          }
+        else {
+        
+          if(!$('#block-yoursearch .searchIntro').hasClass('homeShow')) $('.searchIntro').addClass('homeShow');
+        }
+
+
+
+      }
+      );
      
 
       $('.view-display-id-page_1',context).once('view-solr-search-content').each(function(){
@@ -20,6 +41,7 @@
             if ($('.view-content.row').hasClass('hideRow'))$('.view-content.row').removeClass('hideRow');
             if ($('nav').hasClass('hideRow'))$('nav').removeClass('hideRow');
             if ($('.searchJump').hasClass('searchJump'))$('.view-header').removeClass('searchJump');
+            if($('#sidebar_first').hasClass('facetShow')) $('#sidebar_first').removeClass('facetShow');
 
           }
         else {
@@ -28,6 +50,7 @@
           if(!$('.view-content.row').hasClass('hideRow'))$('.view-content.row').addClass('hideRow');
           if(!$('.view-content.row').hasClass('hideRow'))$('.view-content.row').addClass('hideRow');
           if(!$('nav').hasClass('hideRow'))$('nav').addClass('hideRow');
+          if(!$('#sidebar_first').hasClass('facetShow')) $('#sidebar_first').addClass('facetShow');
         }
 
 
